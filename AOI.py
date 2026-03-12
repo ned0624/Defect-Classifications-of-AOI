efc14f44b421aef5e1f0d8e95dd00b1152edc57e
BING-YI CHEN <147190312+ned0624@users.noreply.github.com>
2026-03-12 17:38:30 +0800

    Add files via upload

---
+++ AOI.py
@@ -0,0 +1,149 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import pandas as pd
+import os
+from torchvision.io import read_image
+import json
+
+class CustomImageDataset(torch.utils.data.Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform != None:
+            image = self.transform(image)
+        if self.target_transform != None:
+            label = self.target_transform(label)
+        return image, label
+
+transform = transforms.Compose(
+    [transforms.ToPILImage(),
+     transforms.ToTensor(),
+     transforms.Normalize((0.5), (0.5))])
+
+batch_size = 16
+
+trainset = CustomImageDataset(annotations_file='./train.csv', img_dir='./train_images/', transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+testset = CustomImageDataset(annotations_file='./test.csv', img_dir='./test_images/', transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=0)
+
+classes = ('0', '1', '2', '3', '4', '5')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# functions to show an image
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = next(dataiter)
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 125 * 125, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 6)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+net.to(device)
+
+for epoch in range(100):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data[0].to(device), data[1].to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % (10) == 1:    # print every 10 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+
+print('Finished Training')
+
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+
+net = Net()
+net.load_state_dict(torch.load(PATH))
+net.to(device)
+
+all_pred = torch.tensor([]).to(device)
+test_csv = pd.read_csv('./test.csv')
+
+# 假設 test_csv 中有一欄 ID 對應每一筆資料
+ids = test_csv['ID']
+
+for data in testloader:
+    images, labels = data[0].to(device), data[1].to(device)
+    outputs = net(images)
+    _, predicted = torch.max(outputs, 1)
+    all_pred = torch.cat((all_pred, predicted), 0)
+
+# 將預測結果轉為 CPU 並轉成 numpy 陣列，同時轉成整數型態
+all_pred_np = all_pred.cpu().numpy().astype(int)
+
+# 創建 DataFrame 並加上 'ID' 和 'Label' 的標頭
+results_df = pd.DataFrame({
+    'ID': ids,
+    'Label': all_pred_np
+})
+
+# 將結果儲存為 CSV 檔
+results_df.to_csv('cnn.csv', index=False)
